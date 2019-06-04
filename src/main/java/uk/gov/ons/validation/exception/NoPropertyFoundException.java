8ec86d2fadca39b08f113edf16dc53f981179eab
Ryan Kilbane <37662781+RyanKilbane@users.noreply.github.com>
2019-06-04 13:19:31 +0100

    Revert "Takeon wrangler lambda lu 2539"

--- src/main/java/uk/gov/ons/validation/exception/NoPropertyFoundException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ons.validation.exception;
-
-public class NoPropertyFoundException extends RuntimeException {
-
-    public NoPropertyFoundException(String message) {
-        super(message);
-    }
-
-}
